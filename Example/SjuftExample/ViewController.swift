96d1abb6afb847f492111cc2b8029899281a2e3b
Mikkel Malmberg <mikkel@brnbw.com>
2015-07-06 22:39:40 +0200

    unlisten in deinit

--- Example/SjuftExample/ViewController.swift
+++ Example/SjuftExample/ViewController.swift
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
         }
     }
     
+    deinit {
+        flux.unlisten(token)
+    }
+    
     @IBAction func increment(sender: AnyObject?) {
         flux.dispatch(CounterActions.increment())
     }
